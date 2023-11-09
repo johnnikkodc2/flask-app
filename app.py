1c7dfc686c821c3fe61f61659d07b32ba6fc6d03
johnnikkodc2 <johnnikko.delacruz.cics@ust.edu.ph>
2023-11-09 13:20:46 +0000

    Intitial Commit

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
+
 from flask import Flask
 
 app = Flask(__name__)
 
-def get_greeting():
-    return "Hello, CI/CD World!"
-
 @app.route('/')
 def hello():
-    return get_greeting()
+    return "Hello, CI/CD World!"
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
--- test_app.py
+++ test_app.py
@@ -1,4 +1,13 @@
-from app import get_greeting
+# File: tests/test_app.py
+from app import app
+import unittest
 
-def test_get_greeting():
-    assert get_greeting() == "Hello, CI/CD World!"
+class FlaskTest(unittest.TestCase):
+    def test_hello(self):
+        tester = app.test_client(self)
+        response = tester.get('/')
+        status_code = response.status_code
+        self.assertEqual(status_code, 200)
+
+if __name__ == '__main__':
+    unittest.main()
